e96361a9fcdb5f90c3b9f76c444eb84bb5439606
David Scott <dscott1414@yahoo.com>
2020-06-11 11:59:25 -0400

    numNoMatch=5948/5696233  0.104% numTotalDifferenceFromStanford=251912

--- tokenize.cpp
+++ tokenize.cpp
@@ -537,7 +537,7 @@ unsigned int Source::doQuotesOwnershipAndContractions(unsigned int &primaryQuota
 				m[q].word->first!=L"let" &&
 				(q+1<lastWord && m[q+1].word!=Words.sectionWord &&
 				// the next word must be "been" or (NOT is and NOT punctuation and NOT 'will').
-				(m[q+1].word->first==L"been" || (m[q+1].queryForm(L"is")<0 && m[q+1].queryForm(L"is_negation")<0 && iswalpha(m[q+1].word->first[0]) && m[q + 1].word->first != L"will"))) &&
+				(m[q+1].word->first==L"been" || (m[q+1].queryForm(L"is")<0 && m[q+1].queryForm(L"is_negation")<0 && iswalpha(m[q+1].word->first[0]) && m[q + 1].word->first != L"will" && m[q + 1].word->first != L"can"))) &&
 				// the previous word must not be "not" / (not Whittington's one) and not "is"
 				(!q || (m[q-1].word->first!=L"not" && m[q-1].queryForm(isForm)<0)))
 			{
